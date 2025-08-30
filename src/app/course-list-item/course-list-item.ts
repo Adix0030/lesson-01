fd06ea53d04396cb670987f23c4a486cadc8816c
Adi Saric <adi.saric02gmail.com>
2025-08-30 13:15:46 +0200

    Add course-list-item component

---
+++ src/app/course-list-item/course-list-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list-item',
+  imports: [],
+  templateUrl: './course-list-item.html',
+  styleUrl: './course-list-item.css'
+})
+export class CourseListItem {
+
+}
